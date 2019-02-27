c305867ed14cec7d73396e6d1457508a4fa5a75e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-02-26 16:53:57 -0800

    Revert "Experimenting with moving Store to TypeScript"
    This reverts commit 4c711adefea13581aef303e8680f86f8b65094e6.

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,16 +1,13 @@
 import { todosReducer } from "./todos/reducer";
 import { visibilityFilterReducer } from "./visibilityFilter/reducer";
 import { StoreType, getDefaultStoreType } from "./StoreType";
-import { Reducer, ReducerAction } from "react";
 
-const rootReducer: Reducer<StoreType, ReducerAction<any>> = (
+export default function(
   state: StoreType = getDefaultStoreType(),
   action: any = {}
-) => {
+) {
   return {
     visibilityFilter: visibilityFilterReducer(state.visibilityFilter, action),
     todos: todosReducer(state.todos, action)
   };
-};
-
-export default rootReducer;
+}
